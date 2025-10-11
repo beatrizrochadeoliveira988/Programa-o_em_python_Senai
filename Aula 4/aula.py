a544989d02042bee8043428a28041a6b832af436
beatrizrochadeoliveira988 <beatrizrochadeoliveira988@gmail.com>
2025-10-11 12:12:19 -0300

    Add files via upload

---
+++ Aula 4/aula.py
@@ -0,0 +1,10 @@
+n1 = 10
+n2 = 20
+
+print(n1 + n2)
+print(n1,'-',n2,'=',n1-n2)
+print(10-2)
+nome=input('digite seu nome:')
+#o input naturalmente gera um texto
+n1 = input('digite um número:')
+n2 = int(input('digite um número:'))
